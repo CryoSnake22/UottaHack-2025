4f20b8bfea82b73e967165dba226c70a763ed6a2
Évence Batobo <126938639+Asvo777@users.noreply.github.com>
2025-01-18 03:47:57 -0500

    Update app.py and config.toml

--- app.py
+++ app.py
@@ -7,9 +7,13 @@ st.title('Streamlit Website')
 # Add some text
 st.write('Hello World!')
 
+st.write({"key": ["value"]})
 
-pd.DataFrame([
+
+data = pd.DataFrame([
     {'a': 1, 'b': 2, 'c': 3},
     {'a': 4, 'b': 5, 'c': 6},
     {'a': 7, 'b': 8, 'c': 9},
 ])
+
+st.write(data)
